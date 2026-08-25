beb15d29f48dd911137a72249c58e9aa3bf6d2e9
enKhaii <khai05227@gmail.com>
2026-08-25 16:57:28 +0800

    Bicycle.cpp and .h added, Booking Module almost done

--- SPC/customer.h
+++ SPC/customer.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "globals.h"
 
+Customer* findCustomerById(vector<Customer>& customers, const string& customerID);
 void customerManagementMenu(vector<Customer>& customers, vector<CustomerLogin>& customerLogins, bool isStaff);
 void registerCustomerPrompt(vector<Customer>& customers, vector<CustomerLogin>& logins);
 
